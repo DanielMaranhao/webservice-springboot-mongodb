93522951bf76cb07033509e5a8fe7c0265dc43d4
DanielMaranhao <danielmarquesmaranhao@gmail.com>
2021-07-24 15:25:09 -0300

    Projeto criado

---
+++ src/main/java/com/company/project/ProjectSpringBootMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.company.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringBootMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringBootMongoDbApplication.class, args);
+	}
+
+}
