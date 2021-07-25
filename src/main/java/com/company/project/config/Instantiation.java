5b592a97a341278922bcd62d718bc7ea15512862
DanielMaranhao <danielmarquesmaranhao@gmail.com>
2021-07-25 15:57:24 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/company/project/config/Instantiation.java
+++ src/main/java/com/company/project/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.company.project.config;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.company.project.domain.Post;
 import com.company.project.domain.User;
+import com.company.project.repositories.PostRepository;
 import com.company.project.repositories.UserRepository;
 
 @Configuration
@@ -15,15 +19,23 @@ public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com");
-		User u3 = new User(null, "Bob Grey", "bob@gmail.com");
+		User u3 = new User(null, "Bob Grey", "bob@gmail.com");		
+		
+		Post p1 = new Post(null, LocalDate.parse("2018-03-21").atStartOfDay(ZoneId.of("GMT")).toInstant(), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", u1);
+		Post p2 = new Post(null, LocalDate.parse("2018-03-23").atStartOfDay(ZoneId.of("GMT")).toInstant(), "Bom dia", "Acordei feliz hoje!", u1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
+		postRepository.saveAll(Arrays.asList(p1, p2));
 	}
 }
