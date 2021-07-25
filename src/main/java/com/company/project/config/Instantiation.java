603155cdca3b81efff80e53c4a6823830a97f34a
DanielMaranhao <danielmarquesmaranhao@gmail.com>
2021-07-25 16:15:30 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/company/project/config/Instantiation.java
+++ src/main/java/com/company/project/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.company.project.domain.Post;
 import com.company.project.domain.User;
+import com.company.project.dto.AuthorDTO;
 import com.company.project.repositories.PostRepository;
 import com.company.project.repositories.UserRepository;
 
@@ -30,12 +31,13 @@ public class Instantiation implements CommandLineRunner {
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com");
-		User u3 = new User(null, "Bob Grey", "bob@gmail.com");		
-		
-		Post p1 = new Post(null, LocalDate.parse("2018-03-21").atStartOfDay(ZoneId.of("GMT")).toInstant(), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", u1);
-		Post p2 = new Post(null, LocalDate.parse("2018-03-23").atStartOfDay(ZoneId.of("GMT")).toInstant(), "Bom dia", "Acordei feliz hoje!", u1);
+		User u3 = new User(null, "Bob Grey", "bob@gmail.com");	
 		
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
+		
+		Post p1 = new Post(null, LocalDate.parse("2018-03-21").atStartOfDay(ZoneId.of("GMT")).toInstant(), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(u1));
+		Post p2 = new Post(null, LocalDate.parse("2018-03-23").atStartOfDay(ZoneId.of("GMT")).toInstant(), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(u1));		
+		
 		postRepository.saveAll(Arrays.asList(p1, p2));
 	}
 }
